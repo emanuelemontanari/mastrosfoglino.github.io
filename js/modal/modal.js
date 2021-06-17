3815cb2d0c8a50456257c43fcb7014e59da54c63
Mastro Sfoglino <75099721+emanuelemontanari@users.noreply.github.com>
2021-06-17 10:42:02 +0200

    Add files via upload

---
+++ js/modal/modal.js
@@ -0,0 +1,46 @@
+// Modal-sfoglina-o
+$('#sfoglina-o-modal').change(function() {
+      if ($(this).is(":checked")) {
+        $('body').addClass('modal-open');
+      } else {
+        $('body').removeClass('modal-open');
+      }
+    });
+    $('#sfoglina-o-modal').change(function() {
+      if ($(this).is(":checked")) {
+        $('.modal-sfoglina-o').addClass('in');
+      } else {
+        $('.modal-sfoglina-o').removeClass('in');
+      }
+    });
+    $('#sfoglina-o-modal').change(function() {
+      if ($(this).is(":checked")) {
+        $('.modal-back').addClass('modal-backdrop');
+      } else {
+        $('.modal-back').removeClass('modal-backdrop');
+      }
+    });
+    
+// Modal-sfoglina-o
+$('#sfoglia-modal').change(function() {
+      if ($(this).is(":checked")) {
+        $('body').addClass('modal-open');
+      } else {
+        $('body').removeClass('modal-open');
+      }
+    });
+    $('#sfoglia-modal').change(function() {
+      if ($(this).is(":checked")) {
+        $('.modal-sfoglia').addClass('in');
+      } else {
+        $('.modal-sfoglia').removeClass('in');
+      }
+    });
+    $('#sfoglia-modal').change(function() {
+      if ($(this).is(":checked")) {
+        $('.modal-back').addClass('modal-backdrop');
+      } else {
+        $('.modal-back').removeClass('modal-backdrop');
+      }
+    });
+    
